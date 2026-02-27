c01eb4110908e19438f452ab86fe0ff52dffd707
José Noel Marenco <45679964+Josenoelmarenco@users.noreply.github.com>
2026-02-27 11:46:45 +0200

    fixing navigate ()

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -14,10 +14,9 @@ const SignupPage = () => {
   const [city, setCity] = useState("");
   const [zipCode, setZipCode] = useState("");
 
-  const navigate = useNavigate();
-
   const submitForm = (e) => {
     e.preventDefault();
+    const navigate = useNavigate();
 
     const newUser = {
       name,
@@ -33,7 +32,37 @@ const SignupPage = () => {
     navigate("/login");
   };
 
+
 return (
+  <section className="bg-indigo-50">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <form onSubmit={submitForm}>
+            <h2 className="text-3xl text-center font-semibold mb-6">Sign Up</h2>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">Full Name</label>
+              <input
+                type="text"
+                className="border rounded w-full py-2 px-3"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Jane Doe"
+              />
+            </div>
+
+            <div className="mb-4">
+              <label className="block text-gray-700 font-bold mb-2">Email</label>
+              <input
+                type="email"
+                className="border rounded w-full py-2 px-3"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="jane@email.com"
+              />
+            </div>
 
   };
 
